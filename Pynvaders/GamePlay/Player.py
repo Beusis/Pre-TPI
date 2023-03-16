60983cfd29ed49824c8f2373602471908a1348b5
Beusis <Loik.MEYLAN@cpnv.ch>
2023-03-16 10:30:01 +0100

    Added movement to Player.py

--- Pynvaders/GamePlay/Player.py
+++ Pynvaders/GamePlay/Player.py
@@ -5,23 +5,23 @@ from random import randint, seed
 from enum import Enum, auto
 
 
-class Player():
+class Player:
     def __init__(self):
-        self.speed = 0
+        self.speed = 0.3
         self._Display = None
         self.sprite = pygame.image.load("./Content/main-spritesheet.png").convert()
         self.rect = pygame.Rect(152, 1, 21, 21)
+        self.position = pygame.Rect(349, 400, 21, 21)
         self.handle_surface = None
         self.clipRect = None
         self.image = None
+        self.clock = pygame.time.Clock()
 
     def on_init(self):
         self.clip()
-        self.on_render()
 
     def on_render(self):
-        pass
-       # self.rect =
+        self.movement()
 
     def clip(self):  # Get a part of the image
         self.handle_surface = self.sprite.copy()  # Sprite that will get process later
@@ -29,3 +29,15 @@ class Player():
         self.handle_surface.set_clip(self.clipRect)  # Clip or you can call cropped
         self.image = self.sprite.subsurface(self.handle_surface.get_clip())  # Get subsurface
         return self.image.copy()  # Return
+
+    def movement(self):
+
+        keys = pygame.key.get_pressed()
+        dt = self.clock.tick(60)
+
+        if keys[pygame.K_LEFT]:
+            # self.speed[0] = -30
+            self.position[0] -= self.speed * dt
+        elif keys[pygame.K_RIGHT]:
+            # self.speed[0] = 30
+            self.position[0] += self.speed * dt
--- Pynvaders/main.py
+++ Pynvaders/main.py
@@ -14,42 +14,44 @@ import GamePlay.Player
 from pathlib import Path
 import os
 
+
 class App:
     _Background = None
+
     def __init__(self):
-        self.size = (1280, 981)
+        self.size = (720, 480)
         self._running = True
         self._Display = None
         self.player = None
+        self.clock = pygame.time.Clock()
 
     def on_init(self):
         script_dir = Path(__file__).parent.absolute()
-        full_path = os.path.join(script_dir,"Content\heic1304c.jpg")
+        full_path = os.path.join(script_dir, r"Content\back.png")
         print(full_path)
 
         pygame.init()
         pygame.event.set_allowed([QUIT, KEYDOWN])
 
         self._Display = pygame.display.set_mode(self.size)
-        self._Background = pygame.image.load(r"C:\Users\Loikm\OneDrive\Documents\GitHub\Pre-TPI\Pynvaders\Content\heic1304c.jpg")
+        self._Background = pygame.image.load(full_path)
         self.player = GamePlay.Player.Player()
-
 
     def on_loop(self):
         self._Display.blit(self._Background, [0, 0])
-        self._Display.blit(self.player.image, self.player.rect)
+        self._Display.blit(self.player.image, self.player.position)
         pygame.display.flip()
-
-
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        self.player.on_render()
+        self.clock.tick(60)
 
     def start_game(self):
         self.on_init()
         self.player.on_init()
         while self._running:
             self.on_loop()
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
 
 
 if True:
